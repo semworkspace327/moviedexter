88de810e7a705ccc1031e2f54934ce20f2e65991
semworkspace327 <semworkspace327@gmail.com>
2025-11-25 16:49:53 +0100

    Delete lib directory

--- lib/models/paged_response.dart
+++
@@ -1,7 +0,0 @@
-class PagedResponse<T> {
-  PagedResponse({required this.page, required this.totalPages, required this.totalResults, required this.results});
-  final int page;
-  final int totalPages;
-  final int totalResults;
-  final List<T> results;
-}
